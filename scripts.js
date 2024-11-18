9e6fdc72c52e84b1e0f3f90d7ff90b80b27a93c6
rizkyngrh23 <rizkynugraha999@gmail.com>
2024-11-18 16:56:45 +0700

    Updated content

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 // Smooth scrolling for navigation links
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
@@ -20,24 +19,28 @@ fetch('https://api.github.com/users/rizkyngrh23/repos')
     .then(response => response.json())
     .then(repos => {
         const projectsList = document.getElementById('projects-list');
-        repos.forEach(repo => {
-            const projectItem = document.createElement('div');
-            projectItem.classList.add('project-item');
+        const excludedRepos = ['rizkyngrh23.github.io', 'rizkyngrh23', 'github-readme-stats']; // Add repo names to exclude
 
-            const projectTitle = document.createElement('h2');
-            const projectLink = document.createElement('a');
-            projectLink.href = repo.html_url;
-            projectLink.target = '_blank';
-            projectLink.textContent = repo.name;
-            projectTitle.appendChild(projectLink);
+        repos
+            .filter(repo => !excludedRepos.includes(repo.name))
+            .forEach(repo => {
+                const projectItem = document.createElement('div');
+                projectItem.classList.add('project-item');
 
-            const projectDescription = document.createElement('p');
-            projectDescription.textContent = repo.description || 'No description available.';
+                const projectTitle = document.createElement('h2');
+                const projectLink = document.createElement('a');
+                projectLink.href = repo.html_url;
+                projectLink.target = '_blank';
+                projectLink.textContent = repo.name;
+                projectTitle.appendChild(projectLink);
 
-            projectItem.appendChild(projectTitle);
-            projectItem.appendChild(projectDescription);
-            projectsList.appendChild(projectItem);
-        });
+                const projectDescription = document.createElement('p');
+                projectDescription.textContent = repo.description || 'No description available.';
+
+                projectItem.appendChild(projectTitle);
+                projectItem.appendChild(projectDescription);
+                projectsList.appendChild(projectItem);
+            });
     })
     .catch(error => console.error('Error fetching repositories:', error));
 
